9629d4a2c910e563ced9fc54556cdb98c9790406
comeonleo <wutexuanleo@outlook.com>
2018-01-31 14:34:01 +0800

    update launch_ss.sh

--- shadowsocks/launch_ss.sh
+++ shadowsocks/launch_ss.sh
@@ -1,3 +1,8 @@
 #！/bin/bash
 #shadow.sh
 sslocal -c /etc/shadowsocks.json
+
+# then add the following to /etc/rc.local, done!
+# sleep 3
+# sudo nohup bash /home/leo/workspace/dotfiles/shadowsocks/launch_ss.sh>/tmp/proxy.log &
+# exit 0
