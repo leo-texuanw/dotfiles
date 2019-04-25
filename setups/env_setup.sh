b9146b7227e89f3d2c21579f84da18f97ed90def
comeonleo <wutexuanleo@outlook.com>
2019-04-25 16:04:18 +1000

    fix gray fg of vim

--- setups/env_setup.sh
+++ setups/env_setup.sh
@@ -1,14 +1,21 @@
 apt-get update
 apt-get install vim
-ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/workspace/dotfiles/vim/vimrc_ubuntu ~/.vimrc
-source ~/.vimrc
+apt-get install git
+apt-get install tmux
+apt-get install build-essential
 
 # Install Vundle and download pluins
 mkdir -p ~/.vim/bundle/
 cd ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git
+
+ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/workspace/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/workspace/dotfiles/tmux/tmux.conf ~/.tmux.conf
+source ~/.vimrc
 vim +PluginInstall +qall
+
+source ~/.vimrc
 
 # download asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
