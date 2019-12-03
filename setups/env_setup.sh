8b9e716fd875d697837f0738cc3d5ee3ffb5fa52
Leo Wu <wutexuanleo@outlook.com>
2019-12-03 17:43:25 +1100

    add zshrc

--- setups/env_setup.sh
+++ setups/env_setup.sh
@@ -9,9 +9,9 @@ apt-get install build-essential
 
 echo "-------------- Soft linking configurations ..."
 ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/workspace/dotfiles/vim/vimrc_ubuntu ~/.vimrc
+ln -s ~/workspace/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/workspace/dotfiles/tmux/tmux.conf ~/.tmux.conf
-source ~/.vimrc
+ln -s ~/workspace/dotfiles/zsh/zshrc ~/.zshrc
 
 # Install Vundle and download pluins
 echo "-------------- Install Vundle plugins ..."
@@ -19,9 +19,6 @@ mkdir -p ~/.vim/bundle/
 cd ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git
 
-ln -s ~/workspace/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/workspace/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/workspace/dotfiles/tmux/tmux.conf ~/.tmux.conf
 source ~/.vimrc
 vim +PluginInstall +qall
 
