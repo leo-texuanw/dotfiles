bdb8d247bba30817b1df39a011b7f199deef51d6
Leo Wu <wutexuanleo@outlook.com>
2020-02-25 23:54:45 +1100

    update: tmux.conf & vimrc

---
+++ setups/asdf.sh
@@ -0,0 +1,6 @@
+# download asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
+
+asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
+asdf install rust 1.34.0
+source /home/texuanw/.asdf/installs/rust/1.34.0/env
